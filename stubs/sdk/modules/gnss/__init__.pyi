51b32f552e9d316ea7a45b380779f1c5e1da907d
GitHub Action <action@github.com>
2025-10-29 10:58:11 +0000

    Update stubs from EPS-Works/sbc-sdk@7f2920999c4735485cf2520e9a8a1b5c8e7c61cf

--- stubs/sdk/modules/gnss/__init__.pyi
+++ stubs/sdk/modules/gnss/__init__.pyi
@@ -1,10 +1,9 @@
-from .factory import GNSSFactory as GNSSFactory
 from .gnss import GNSS as GNSS
 from .ublox import Ublox as Ublox
 from _typeshed import Incomplete
 from typing import NamedTuple
 
-__all__ = ['GNSS', 'Ublox', 'GNSSFactory', 'GNSS_MODULES']
+__all__ = ['GNSS', 'Ublox', 'GNSS_MODULES']
 
 class GNSSModules(NamedTuple):
     ZED_F9P: Incomplete
